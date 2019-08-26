52cf2902b0d57480ac74a6608d6f6cbe84a29a00
atdaunais <atdaunais@gmail.com>
2019-08-26 15:38:55 -0500

    Completed and tested assignment and challenge

---
+++ graded.py
@@ -0,0 +1,61 @@
+def main():
+    printNumbers()
+    password = input("Enter a password: ")
+    confirm_pass = input("Please confirm your password: ")
+    print(checkPassword(password, confirm_pass))
+    print(evenOrOdd())
+    print(prob4_main(greeting_word()))
+    print(quit_func())
+    print(receiveNums())
+
+
+def printNumbers():
+    for index in range(-25, 21, 1):
+        print(index)
+
+
+def checkPassword(orig_pass, confirm):
+    if orig_pass == confirm:
+        return True
+    else:
+        return False
+
+
+def evenOrOdd():
+    userInput = int(input("Enter a number. I will say if it's even or odd: "))
+    if (userInput % 2) == 0:
+        return f"{userInput} is an even number!"
+    elif (userInput % 2) == 1:
+        return f"{userInput} is an odd number!"
+
+
+def prob4_main(input):
+    return phrase_func(input)
+
+
+def greeting_word():
+    return input("Enter a greeting word: ")
+
+
+def phrase_func(greeting):
+    return f"{greeting} World!"
+
+
+def quit_func():
+    userQuit = ""
+    while userQuit != "q":
+        userQuit = input("Say anything. Press 'q' to quit: ")
+        if userQuit == "q":
+            return "Goodbye!"
+
+
+def receiveNums():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    return f"The sum of {num1} + {num2} is: {num1 + num2}\n" \
+           f"The difference of {num1} - {num2} is: {num1 - num2}\n" \
+           f"The product of {num1} * {num2} is {num1 * num2}\n" \
+           f"The quotient of {num1}/{num2} is: {num1 / num2}"
+
+
+main()
